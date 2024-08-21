6bb5fadd0488100cacb58dd096bc424db1e5a56b
NguyenHongSon4 <sontien1314@gmail.com>
2024-08-21 10:35:48 +0700

    In ra bang cuu chuong

--- Baitap01/Baitap01/obj/Release/net8.0/Baitap01.AssemblyInfo.cs
+++ Baitap01/Baitap01/obj/Release/net8.0/Baitap01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Baitap01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b053fadae73b021323b6c97018f9aca2ceacecc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Baitap01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Baitap01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
