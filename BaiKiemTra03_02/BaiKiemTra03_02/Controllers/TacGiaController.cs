d3d7759b611b8ceacab3402f382368af32f20586
NguyenHongSon4 <sontien1314@gmail.com>
2024-10-07 16:03:41 +0700

    Bai kiem tra 03

---
+++ BaiKiemTra03_02/BaiKiemTra03_02/Controllers/TacGiaController.cs
@@ -0,0 +1,129 @@
+﻿using BaiKiemTra03_02.Data;
+using BaiKiemTra03_02.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiKiemTra03_02.Controllers
+{
+    
+    public class TacGiaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public TacGiaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var tacgia = _db.TacGia.ToList();
+            ViewBag.TacGia = tacgia;
+            return View();
+            
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(TacGia tacGia)
+        {
+            if (ModelState.IsValid)
+            {
+                //them thong tin
+                _db.TacGia.Add(tacGia);
+                //luu lai thong tin
+                _db.SaveChanges();
+                //chuyen ve trang index
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            var tacgia = _db.TacGia.Find(id);
+            if (tacgia == null)
+            {
+                return NotFound();
+            }
+            return View(tacgia);
+
+        }
+        [HttpPost]
+        public IActionResult Edit(TacGia tacGia)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.TacGia.Update(tacGia);
+                _db.SaveChanges();
+                return RedirectToAction("Details", new { id = tacGia.AuthorId }); // Redirect to Details action
+            }
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            var tacgia = _db.TacGia.Find(id);
+            return View(tacgia);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteConfirm(int id)
+        {
+            var tacgia = _db.TacGia.Find(id);
+            if (tacgia == null)
+            {
+                return NotFound();
+            }
+            _db.TacGia.Remove(tacgia);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            var tacgia = _db.TacGia.Find(id);
+            return View(tacgia);
+        }
+
+        [HttpGet]
+        public IActionResult Search(string searchString)
+        {
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                //use linq
+                var tacgia = _db.TacGia
+                    .Where(tl => tl.AuthorName.Contains(searchString))
+                    .ToList();
+                ViewBag.SearchString = searchString;
+                ViewBag.TacGia = tacgia;
+            }
+            else
+            {
+                var tacgia = _db.TacGia.ToList();
+                ViewBag.TacGia = tacgia;
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ BaiKiemTra03_02/BaiKiemTra03_02/Models/TacGia.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BaiKiemTra03_02.Models
+{
+    public class TacGia
+    {
+        [Key]
+        public int AuthorId { get; set; }
+
+        [Required(ErrorMessage = "Tên tác giả là bắt buộc.")]
+        [StringLength(100, ErrorMessage = "Tên tác giả không được dài hơn 99 ký tự.")]
+        public string AuthorName { get; set; }
+
+        [Required(ErrorMessage = "Quốc tịch là bắt buộc.")]
+        public string Nationality { get; set; }
+
+        [Required(ErrorMessage = "Năm sinh là bắt buộc.")]
+        public int BirthYear { get; set; } 
+    }
+
+}
